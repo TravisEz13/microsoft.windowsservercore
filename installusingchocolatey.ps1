0b8fa23d6bfbbfe214b187de049bd0bedb7762e6
Travis Plunk <travis.plunk@microsoft.com>
2017-04-07 09:51:02 -0700

    switch back to using Chocolatey command line

--- installusingchocolatey.ps1
+++ installusingchocolatey.ps1
@@ -2,39 +2,53 @@ param(
     [Parameter(Mandatory=$true)]
     [string]
     $PackageName,
-    [Parameter(Mandatory=$true)]
+
+    [Parameter(Mandatory=$false)]
     [string]
-    $executable
+    $Executable
+    ,
+    [switch]
+    $Cleanup
 )
 
-if(-not( Get-PackageProvider -Name Chocolatey -ErrorAction SilentlyContinue))
+if(-not(Get-Command -name Choco -ErrorAction SilentlyContinue))
 {
     Write-Verbose "Installing Chocolatey provider..." -Verbose
-    Install-PackageProvider -Name Chocolatey
+    Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 }
 
 Write-Verbose "Installing $PackageName..." -Verbose
-Install-Package -Name $PackageName -ProviderName Chocolatey -Force
+choco install -y $PackageName
 
-Write-Verbose "Verifing $Executable is in path..." -Verbose
-$exeSource = $null
-$exeSource = Get-ChildItem -path "$env:ProgramFiles\$executable" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty FullName
-if(!$exeSource)
+if($executable)
 {
-    Write-Verbose "Falling back to x86 program files..." -Verbose
-    $exeSource = Get-ChildItem -path "${env:ProgramFiles(x86)}\$executable" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty FullName
-}
-if(!$exeSource)
-{
-    Write-Verbose "Falling back to the root of the drive..." -Verbose
-    $exeSource = Get-ChildItem -path "/$executable" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty FullName
-}
-if(!$exeSource)
-{
-    throw "$executable not found"
+    Write-Verbose "Verifing $Executable is in path..." -Verbose
+    $exeSource = $null
+    $exeSource = Get-ChildItem -path "$env:ProgramFiles\$Executable" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty FullName
+    if(!$exeSource)
+    {
+        Write-Verbose "Falling back to x86 program files..." -Verbose
+        $exeSource = Get-ChildItem -path "${env:ProgramFiles(x86)}\$Executable" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty FullName
+    }
+    if(!$exeSource)
+    {
+        Write-Verbose "Falling back to the root of the drive..." -Verbose
+        $exeSource = Get-ChildItem -path "/$Executable" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty FullName
+    }
+    if(!$exeSource)
+    {
+        throw "$Executable not found"
+    }
+
+    $exePath = Split-Path -Path $exeSource
+    $newPath = "$env:path;$exePath"
+    Write-Verbose "setting path to: $newPath" -Verbose
+    [System.Environment]::SetEnvironmentVariable('path',$newPath,[System.EnvironmentVariableTarget]::Machine)
 }
 
-$exePath = Split-Path -Path $exeSource
-$newPath = "$env:path;$exePath"
-Write-Verbose "setting path to: $newPath" -Verbose
-[System.Environment]::SetEnvironmentVariable('path',$newPath,[System.EnvironmentVariableTarget]::Machine)
+if($Cleanup.IsPresent)
+{
+    Write-Verbose "Cleaning up chocolatey..." -Verbose
+    Remove-Item -recurse -force $env:temp\chocolatey -ErrorAction SilentlyContinue
+    remove-item -Recurse "$env:ProgramData\chocolatey\lib"  -ErrorAction SilentlyContinue
+}
