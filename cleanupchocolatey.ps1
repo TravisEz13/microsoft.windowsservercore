1854bf72210616b8d611a723ad107fb6434b352f
Travis Plunk <travis.plunk@microsoft.com>
2017-04-07 09:58:43 -0700

    remove unused files

--- cleanupchocolatey.ps1
+++
@@ -1,3 +0,0 @@
-Write-Verbose "Cleaning up..." -Verbose
-Remove-Item -recurse -force $env:temp\chocolatey -ErrorAction SilentlyContinue
-remove-item -Recurse "$env:ProgramData\chocolatey\lib"  -ErrorAction SilentlyContinue
