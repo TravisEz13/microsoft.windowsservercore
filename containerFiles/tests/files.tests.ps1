4204a353768e27243d74c0cf5aafb276642744fa
Travis Plunk <travis.plunk@microsoft.com>
2017-04-13 18:55:29 -0700

    add test for WIX

---
+++ containerFiles/tests/files.tests.ps1
@@ -0,0 +1,31 @@
+function Get-File
+{
+    param(
+        $filter
+    )
+
+    $result = Get-ChildItem $filter -ErrorAction SilentlyContinue -Recurse | Select-Object -First 1
+    if($result)
+    {
+        return $result.fullname
+    }
+    return $filter
+}
+
+Describe "Verify containers contain expected files" {
+    $repos = @(
+        @{
+            file = (Get-File -filter "${env:ProgramFiles(x86)}\WiX Toolset v3.10\bin\heat.exe")
+        }
+    )
+
+    it "should contain <file>" -TestCases $repos {
+        param(
+            [parameter(Mandatory)]
+            [string]$file
+            
+        )
+
+        $file | should exist
+    }
+}
